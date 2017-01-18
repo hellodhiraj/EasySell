4d8fab9fe5253964be6f69504b30188f84b11a11
gurpreethacker <mca.gsm@hotmail.com>
2017-01-18 11:33:40 -0500

    Created stringPower skeleton

---
+++ src/csd4464/assignment1/easysell/CSD4464Assignment1EasySell.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csd4464.assignment1.easysell;
+
+/**
+ *
+ * @author c0691289
+ */
+public class CSD4464Assignment1EasySell {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    }
+
+    public static String stringPower(String s, int i) {
+        return s;
+    }
+}
