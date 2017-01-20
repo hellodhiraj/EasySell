7ad2d00909e5e048caad074ee84cf86e0b401dd2
anter123 <anter_singh2001@yahoo.com>
2017-01-20 16:06:52 -0500

    "Created testLessThanOne and testStringAndOne methods."

--- test/csd4464/assignment1/easysell/CSD4464Assignment1EasySellTest.java
+++ test/csd4464/assignment1/easysell/CSD4464Assignment1EasySellTest.java
@@ -46,34 +46,42 @@ public class CSD4464Assignment1EasySellTest {
         String[] args = null;
         CSD4464Assignment1EasySell.main(args);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+      
     }
 
     /**
      * Test of stringPower method, of class CSD4464Assignment1EasySell.
      */
-    @Test
-    public void testStringPower() {
-        System.out.println("stringPower");
-        String s = "";
-        int i = 0;
-        String expResult = "";
-        String result = CSD4464Assignment1EasySell.stringPower(s, i);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    
     @Test
     public void testEmptyStringShouldReturnEmptyString() {
         System.out.println("stringPower");
-        String s = "";
-        int i = 0;
-        String expResult = "";
+        String s ="";
+        int i = 9;
+        String expResult ="";
         String result = CSD4464Assignment1EasySell.stringPower(s, i);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
+    public void  testLessThanOneShouldReturnNull(){
+    System.out.println("stringPower");
+        String s = "";
+        int i = 0;
+        String expResult = null;
+        String result = CSD4464Assignment1EasySell.stringPower(s, i);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      
+}
+    public void  testStringAndOneShouldReturnString(){
+    System.out.println("stringPower");
+        String s ="";
+        int i = 1;
+        String expResult ="d";
+        String result = CSD4464Assignment1EasySell.stringPower(s, i);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      
     
-    
+}
 }
